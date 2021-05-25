b53b9bbae9ae301c46f376d6f0c99769603e1879
integral <volkovanton71@yandex.ru>
2021-05-25 16:57:21 +0300

    merge

--- src/service/service.js
+++ src/service/service.js
@@ -11,8 +11,7 @@ const userArguments = process.argv.slice(USER_ARGV_INDEX);
 const [userCommand] = userArguments;
 
 if (userArguments.length === 0 || !Cli[userCommand]) {
-  Cli[DEFAULT_COMMAND].run();
-  process.exit(ExitCode.success);
+  return Cli[DEFAULT_COMMAND].run();
 }
 
-Cli[userCommand].run(userArguments.slice(1));
+return Cli[userCommand].run(userArguments.slice(1));
