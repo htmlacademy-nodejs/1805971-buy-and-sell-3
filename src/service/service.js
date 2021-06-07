cff151943cb6ba5031b54b07720e0334062749c4
Anton <61318805+AntonVolkov71@users.noreply.github.com>
2021-06-07 16:00:34 +0300

    Учебный проект: асинхронность. Побег из ада коллбеков (#4)
    * fix git merges
    
    * merge
    
    * chalk && read file
    
    * fix eslint
    
    * git fix changes

--- src/service/service.js
+++ src/service/service.js
@@ -4,16 +4,13 @@ const {Cli} = require(`./cli`);
 const {
   DEFAULT_COMMAND,
   USER_ARGV_INDEX,
-  ExitCode
 } = require(`../constants`);
 
 const userArguments = process.argv.slice(USER_ARGV_INDEX);
 const [userCommand] = userArguments;
 
 if (userArguments.length === 0 || !Cli[userCommand]) {
-  Cli[DEFAULT_COMMAND].run();
-  process.exit(ExitCode.success);
+  return Cli[DEFAULT_COMMAND].run();
 }
 
-Cli[userCommand].run(userArguments.slice(1));
-
+return Cli[userCommand].run(userArguments.slice(1));
