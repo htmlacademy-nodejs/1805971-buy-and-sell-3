3f31e1c7df31eb3360912582df6b569d67d22da8
integral <volkovanton71@yandex.ru>
2021-06-27 17:44:28 +0300

    module2 task3 goto review

--- src/constants.js
+++ src/constants.js
@@ -11,3 +11,11 @@ const ExitCode = {
 module.exports = {
   DEFAULT_COMMAND, USER_ARGV_INDEX, ExitCode
 };
+
+module.exports.HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
