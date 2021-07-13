f9f41ed0beaa5bb6a22f4473cb7a4992753c68ce
integral <volkovanton71@yandex.ru>
2021-07-13 21:43:09 +0300

    module3-task2

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -3,7 +3,7 @@
 const {Router} = require(`express`);
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.send(`/my`));
-myRouter.get(`/comments`, (req, res) => res.send(`/my/comments`));
+myRouter.get(`/`, (req, res) => res.render(`offers/my-tickets`));
+myRouter.get(`/comments`, (req, res) => res.render(`comments/comments`));
 
 module.exports = myRouter;
