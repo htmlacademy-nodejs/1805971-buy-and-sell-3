8497adeb48a164210b383b7697c3a5929b58d1f5
integral <volkovanton71@yandex.ru>
2021-07-28 14:41:23 +0300

    module4-task1

---
+++ src/service/api/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const {Router} = require(`express`);
+const category = require(`../api/category`);
+const search = require(`../api/search`);
+const comment = require(`../api/comment`);
+const offer = require(`../api/offer`);
+
+// const user = require(`../api/user`);
+
+const getMockData = require(`../lib/get-mock-data`);
+
+const {
+  CategoryService,
+  SearchService,
+  OfferService,
+  CommentService,
+} = require(`../data-service`);
+
+const app = new Router();
+
+(async () => {
+  const mockData = await getMockData();
+
+  category(app, new CategoryService(mockData));
+  search(app, new SearchService(mockData));
+  comment(app, new CommentService(mockData));
+  offer(app, new OfferService(mockData), new CommentService(mockData));
+})();
+
+module.exports = app;
--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -3,6 +3,7 @@
 const chalk = require(`chalk`);
 const fs = require(`fs`).promises;
 const path = require(`path`);
+const {nanoid} = require(`nanoid`);
 
 const {
   getRandomInt,
@@ -18,6 +19,7 @@ const FILE_NAME = `mocks.json`;
 const FILE_SENTENCES_PATH = `../../data/sentences.txt`;
 const FILE_TITLES_PATH = `../../data/titles.txt`;
 const FILE_CATEGORIES_PATH = `../../data/categories.txt`;
+const FILE_COMMENTS_PATH = `../../data/comments.txt`;
 
 const OfferType = {
   OFFER: `offer`,
@@ -34,6 +36,9 @@ const PictureRestrict = {
   MAX: 16
 };
 
+const MAX_ID_LENGTH = 6;
+const MAX_COMMENTS = 4;
+
 const readContent = async (filePath) => {
   const absolutePath = path.join(__dirname, filePath);
 
@@ -46,14 +51,25 @@ const readContent = async (filePath) => {
   }
 };
 
-const generateOffers = (count, titles, categories, sentences) => (
+const generateComments = (count, comments) => (
   Array(count).fill({}).map(() => ({
+    id: nanoid(MAX_ID_LENGTH),
+    text: shuffle(comments)
+      .slice(0, getRandomInt(1, 3))
+      .join(` `),
+  }))
+);
+
+const generateOffers = ({count, titles, categories, sentences, comments}) => (
+  Array(count).fill({}).map(() => ({
+    id: nanoid(MAX_ID_LENGTH),
     category: [categories[getRandomInt(0, categories.length - 1)]],
     description: shuffle(sentences).slice(1, 5).join(` `),
     picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
     title: titles[getRandomInt(0, titles.length - 1)],
     type: OfferType[Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)]],
     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
+    comments: generateComments(getRandomInt(1, MAX_COMMENTS), comments)
   }))
 );
 
@@ -70,9 +86,10 @@ module.exports = {
     const sentences = await readContent(FILE_SENTENCES_PATH);
     const titles = await readContent(FILE_TITLES_PATH);
     const categories = await readContent(FILE_CATEGORIES_PATH);
+    const comments = await readContent(FILE_COMMENTS_PATH);
 
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences));
+    const content = JSON.stringify(generateOffers({count: countOffer, titles, categories, sentences, comments}));
 
     try {
       await fs.writeFile(FILE_NAME, content);
